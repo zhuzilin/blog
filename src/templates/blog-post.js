1f61df0642d2331b0d39f7af051543f65c066efc
zhuzilin <zhuzilinallen@gmail.com>
2019-01-31 00:17:01 -0500

    change quote font size to inherit

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,7 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
+import './blog-post.css'
 require(`katex/dist/katex.min.css`)
 
 export default ({ data }) => {
