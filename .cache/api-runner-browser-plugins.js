c8fbbd49bb1e7de5233227f16fb60e80477caf28
zhuzilin <zhuzilinallen@gmail.com>
2019-07-19 15:23:10 -0700

    clean master branch

---
+++ .cache/api-runner-browser-plugins.js
@@ -0,0 +1,7 @@
+module.exports = [{
+      plugin: require('/home/zilzhu/Documents/Git/zhuzilin.github.io/node_modules/gatsby-plugin-typography/gatsby-browser.js'),
+      options: {"plugins":[],"pathToConfigModule":"src/utils/typography"},
+    },{
+      plugin: require('/home/zilzhu/Documents/Git/zhuzilin.github.io/gatsby-browser.js'),
+      options: {"plugins":[]},
+    }]
