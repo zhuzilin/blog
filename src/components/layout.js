5a852c1b4b39a52eaa406ebf89930190456f779a
zilinzhu <zilinzhu@tencent.com>
2021-02-22 10:50:10 +0800

    change indentation

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,42 +5,41 @@ import { Helmet } from "react-helmet"
 import { rhythm } from "../utils/typography"
 
 export default ({ children }) => (
-    <StaticQuery
-        query={
-          graphql`
-            query {
-              site {
-                siteMetadata {
-                  title
-                }
-              }
-            }
-          `}
-    render={data => (
-      <div css={css`
-                margin: 0 auto;
-                max-width: 900px;
-                padding: ${rhythm(2)};
-                padding-top: ${rhythm(1.5)};`}>
-          <Helmet>
-              <meta charSet="utf-8" />
-              <title>Zilin's Blog</title>
-          </Helmet>
-          <Link to={`/`}>
-              <h3 css={css`
-                      margin-bottom: ${rhythm(2)};
-                      display: inline-block;
-                      font-style: normal;`}>
-                  {data.site.siteMetadata.title}
-              </h3>
-          </Link>
-          <Link to={`/about/`}
-                css={css`
-                    float: right;`}>
-              about
-          </Link>
-          {children}
-      </div>
+  <StaticQuery
+    query={
+      graphql`
+      query {
+        site {
+        siteMetadata {
+          title
+        }
+        }
+      }
+      `}
+  render={data => (
+    <div css={css`
+        margin: 0 auto;
+        max-width: 900px;
+        padding: ${rhythm(2)};
+        padding-top: ${rhythm(1.5)};`}>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>zhuzilin's Blog</title>
+      </Helmet>
+      <Link to={`/`}>
+        <h3 css={css`
+            margin-bottom: ${rhythm(2)};
+            display: inline-block;
+            font-style: normal;`}>
+          {data.site.siteMetadata.title}
+        </h3>
+      </Link>
+      <Link to={`/about/`}
+        css={css`float: right;`}>
+        about
+      </Link>
+      {children}
+    </div>
   )}
   />
 )
